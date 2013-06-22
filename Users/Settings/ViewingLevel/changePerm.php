4ea54e8186affdff8c2d82ec4c2c34e314f838e5
MiteyIronPaw <grayghost.ingress@gmail.com>
2013-06-22 16:04:05 +1200

    Fixed bug preventing setting viewing level in area

--- Users/Settings/ViewingLevel/changePerm.php
+++ Users/Settings/ViewingLevel/changePerm.php
@@ -7,6 +7,7 @@
 		include "/var/www/Ingress/Tools/database.php";
 
 		$sql = "UPDATE AgentTable SET InLvl = '".$AreaLvl."' WHERE username = '".$_SESSION['name']."'";
+		mysqli_query($con,$sql);
 		$sql = "UPDATE AgentTable SET outLvl = '".$AnyLvl."' WHERE username = '".$_SESSION['name']."'";
 		mysqli_query($con,$sql);
 
