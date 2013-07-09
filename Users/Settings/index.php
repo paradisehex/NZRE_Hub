ce17beb64ca7ac2cde8d63f0a8b0eb1daf1bcbde
MiteyIronPaw <grayghost.ingress@gmail.com>
2013-07-09 12:20:56 +1200

    Some Tweeks

--- Users/Settings/index.php
+++ Users/Settings/index.php
@@ -10,13 +10,6 @@
 	<body>
 		<?php include "/var/www/Ingress/Tools/menu.php";?>
 		<br>
-		<form action="changepw.php" method="post" autocomplete="off">
-			<div id="line">Change Password</div>
-			<div id="line"><input class="field" type="password" name="OldPassword" placeholder="Old Password"></div>
-			<div id="line"><input class="field" type="password" name="NewPassword" placeholder="New Password"></div>
-			<div id="line"><input class="field" type="password" name="NewPassword2" placeholder="Confirm Password"></div>
-			<div id="line"><input class="button" type="submit" value="Change" ></div>
-		</form>
 
 
 		<form action="changeAP.php" method="post" autocomplete="off">
@@ -36,6 +29,18 @@
 				echo "</form>";
 			}
 		?>
+
+
+		<form action="changepw.php" method="post" autocomplete="off">
+			<div id="line">Change Password</div>
+			<div id="line"><input class="field" type="password" name="OldPassword" placeholder="Old Password"></div>
+			<div id="line"><input class="field" type="password" name="NewPassword" placeholder="New Password"></div>
+			<div id="line"><input class="field" type="password" name="NewPassword2" placeholder="Confirm Password"></div>
+			<div id="line"><input class="button" type="submit" value="Change" ></div>
+		</form>
+
+
+
 		<div id="line">
 			<a href="ViewingLevel">Set inventory viewing permissions</a>
 		</div>
