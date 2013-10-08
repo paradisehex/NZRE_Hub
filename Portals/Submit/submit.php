c3f1ab1f2d82ba986f9183d25cc4eb200bfb8ff0
MiteyIronPaw <grayghost.ingress@gmail.com>
2013-10-08 17:40:31 +1300

    Removed area's from portals because it wasn't useful

--- Portals/Submit/submit.php
+++ Portals/Submit/submit.php
@@ -4,7 +4,6 @@
 		include "/var/www/Ingress/Tools/permission.php";
 
 		$Name = strip_tags(stripslashes($_POST['Name']));
-		$Location = strip_tags(stripslashes($_POST['Location']));
 		$Latitude = doubleval(strip_tags($_POST['Latitude']))*1000000;
 		$Longitude = doubleval(strip_tags($_POST['Longitude']))*1000000;
 
@@ -14,7 +13,7 @@
 			$count = mysqli_num_rows($result);
 			echo $count;
 			if($count == 0){
-				mysqli_query($con,"insert into PortalTable values('$Name','$Location','$Latitude','$Longitude');");
+				mysqli_query($con,"insert into PortalTable values('$Name','$Latitude','$Longitude');");
 				header("location:../");
 			}else{
 				echo "Portal already added";
