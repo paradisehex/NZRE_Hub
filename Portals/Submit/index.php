956ad58bf4f239cdc3b32b2f8f1d4cfe6f705d45
MiteyIronPaw <grayghost.ingress@gmail.com>
2013-10-11 21:08:37 +1300

    Reverted to new layout tweak after user feedback

--- Portals/Submit/index.php
+++ Portals/Submit/index.php
@@ -11,6 +11,26 @@
 				Submit Portal<br>
 				<input class="field" type="text" name="Name" autocomplete="off" placeholder="Portal Name"autocomplete="off"><br>
 				<input class="field" type="text" name="Location" autocomplete="off" placeholder="Location ID (Northland is 1)" autocomplete="off"><br>
+				<div id="hovButton">
+					Location
+					<div id="hovButtonHolder">
+						<input class="button" type="submit" value="Northland" >
+						<input class="button" type="submit" value="Auckland" >
+						<input class="button" type="submit" value="Waikato" >
+						<input class="button" type="submit" value="Bay of Plenty" >
+						<input class="button" type="submit" value="Gisborne" >
+						<input class="button" type="submit" value="Hawke's Bay" >
+						<input class="button" type="submit" value="Taranaki" >
+						<input class="button" type="submit" value="Manawatu-Wanganui" >
+						<input class="button" type="submit" value="Wellington" >
+						<input class="button" type="submit" value="Nelson-Tasman" >
+						<input class="button" type="submit" value="Marlborough" >
+						<input class="button" type="submit" value="West Coast" >
+						<input class="button" type="submit" value="Canterbury" >
+						<input class="button" type="submit" value="Otago" >
+						<input class="button" type="submit" value="Southland" >
+					</div>
+				</div>
 				<input class="field" type="text" name="Latitude" placeholder="Latitude" autocomplete="off"><br>
 				<input class="field" type="text" name="Longitude" placeholder="Longitude" autocomplete="off"><br>
 				<input class="button" type="submit" value="Submit" >
