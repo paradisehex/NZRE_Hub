976744f80128eaf4f9d00af76f5e2c39c6e73545
MiteyIronPaw <grayghost.ingress@gmail.com>
2013-10-17 14:17:54 +1300

    Fixed portal keys not deleting

--- Portals/Info/delete.php
+++ Portals/Info/delete.php
@@ -3,13 +3,14 @@
 	
 	include "/var/www/Ingress/Tools/database.php";
 	include "/var/www/Ingress/Tools/permission.php";
+	include "/var/www/Ingress/Tools/getPortalInfo.php";
 		
 	if(IsOfficer($con,$_SESSION['name'])){
 		
 		$Name = strip_tags(stripslashes($_POST['Name']));
 		
 		mysqli_query($con,"delete from PortalTable WHERE portalName = '".$Name."'");
-		mysqli_query($con,"delete from KeyTable WHERE portalName = '".$Name."'");
+		mysqli_query($con,"delete from KeyTable WHERE portalName = '".getPortalID($Name)."'");
 			
 		header("location:../");
 	}else{
