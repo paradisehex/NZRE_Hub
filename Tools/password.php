fff3bd547979973b4cbcfa9508d2b5a9903e8db4
MiteyIronPaw <grayghost.ingress@gmail.com>
2013-11-08 19:09:18 +1300

    Removed the old password hash function

--- Tools/password.php
+++ Tools/password.php
@@ -1,38 +1,15 @@
 <?php
-	function checkPassword($Name,$Password,$con){
-		$OldPassword = md5($Password);
-		$NewPassword =getHash($Name,$Password);
+	function checkPassword($Name, $Password, $con){
+		$PasswordHash = getHash($Name, $Password);
 
-		$OldSQL = "SELECT * FROM AgentTable WHERE username='$Name' and password='$OldPassword'";
-		$NewSQL = "SELECT * FROM AgentTable WHERE username='$Name' and passwordHash='$NewPassword'";
+		$SQL = "SELECT * FROM AgentTable WHERE username='$Name' and passwordHash='$PasswordHash'";
 
-		$OldResult = mysqli_query($con,$OldSQL);
-		$NewResult = mysqli_query($con,$NewSQL);
+		$Result = mysqli_query($con,$SQL);
 
-		$Oldcount = mysqli_num_rows($OldResult);
-		$Newcount = mysqli_num_rows($NewResult);
+		$count = mysqli_num_rows($Result);
 
-		//echo $NewPassword."<br>";
-
-
-		if($Oldcount==1){
-			$row = mysqli_fetch_array($OldResult);
-			$_SESSION['name'] = $row['username'];
-			$_SESSION['admin'] = $row['Admin'];
-			$_SESSION['lvl'] = $row['lvl'];
-			
-			$sql2 = "UPDATE AgentTable SET password = '' WHERE username = '".$Name."'";
-			mysqli_query($con,$sql2);
-
-			$sql2 = "UPDATE AgentTable SET passwordHash = '".$NewPassword."' WHERE username = '".$Name."'";
-			mysqli_query($con,$sql2);
-
-			return true;
-		}
-
-
-		if($Newcount==1){
-			$row = mysqli_fetch_array($NewResult);
+		if($count==1){
+			$row = mysqli_fetch_array($Result);
 			$_SESSION['name'] = $row['username'];
 			$_SESSION['admin'] = $row['Admin'];
 			$_SESSION['lvl'] = $row['lvl'];
@@ -42,11 +19,8 @@
 
 		return false;
 	}
-
-
-
-
-	function getHash($Name,$Password){
-		return hash('whirlpool',md5($Name).$Password);
+	
+	function getHash($Name, $Password){
+		return hash('whirlpool', md5($Name).$Password);
 	}
 ?>
--- Tools/register.php
+++ Tools/register.php
@@ -4,7 +4,7 @@
 	function register($con,$username,$password,$level){
 		$password = getHash($username,$password);
 		//Add password and user to data base
-			mysqli_query($con,"insert into AgentTable values('$username','','$password',false,$level,0,0,7,7);");
+			mysqli_query($con,"insert into AgentTable values('$username','$password',false,$level,0,0,7,7);");
 			mysqli_query($con,"insert into ItemTable values('$username',0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'Never',0);");
 		//Go to tools page
 			LogText("User ".$_SESSION['name']." Registered ".$username);
