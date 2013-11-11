f8a10e573692335a919b3159ee3973c6bd132d2d
MiteyIronPaw <grayghost.ingress@gmail.com>
2013-11-11 17:17:21 +1300

    Disabled the log system while trying to make everything compatable with the new server

--- Tools/log.php
+++ Tools/log.php
@@ -1,8 +1,8 @@
 <?php
 	function LogText($String){
-		$file = $_SESSION['path'].'/.data/Log.txt';
-		$output = date('H',time()).":".date('i',time())." ".date('d',time())."/".date('m',time())."-".$String."\n";
-		$output .= file_get_contents($file);
-		file_put_contents($file, $output,LOCK_EX);
+		//$file = $_SESSION['path'].'/.data/Log.txt';
+		//$output = date('H',time()).":".date('i',time())." ".date('d',time())."/".date('m',time())."-".$String."\n";
+		//$output .= file_get_contents($file);
+		//file_put_contents($file, $output,LOCK_EX);
 	}
 ?>
