a0790e5b7cdee807f0249d495a0f3bf8683aff67
MiteyIronPaw <grayghost.ingress@gmail.com>
2013-12-17 17:23:41 +1300

    Fixed register not working on some servers

--- Tools/register.php
+++ Tools/register.php
@@ -4,7 +4,7 @@
 	function register($username,$password,$level){
 		$password = getHash($username,$password);
 		//Add password and user to data base
-			insert("AgentTable",array($username,$password,false,$level,0,0,7,7));
+			insertCertainVaules("AgentTable", array("username", "passwordHash", "Admin", "lvl", "AP", "Location", "InLvl", "outLvl"), array($username,$password,false,$level,0,0,7,7));
 			insert("ItemTable",array($username,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'Never',0));
 		//Go to tools page
 			LogText("User ".$_SESSION['name']." Registered ".$username);
