ae67bea4c9b7a835daf1f7acb1c5f847e2b31222
MiteyIronPaw <grayghost.ingress@gmail.com>
2013-12-23 16:57:12 +1300

    Moved the CSS to a more logical place

--- Tools/head.php
+++ Tools/head.php
@@ -1,12 +1,12 @@
-<?php
-	echo "<head>";	
-	echo "<title>Resistance</title>";
-	echo "<meta http-equiv=\"Content-Language\" content=\"en\"/>";
-	if($_SESSION['view']=="Desktop"){
-		echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"/Ingress/Users/style.css\">";
-	}else{
-		echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"/Ingress/Users/mobile.css\">";
-	}
-	echo "<link rel=\"shortcut icon\" href=\"/Ingress/icon.png\" />";
-	echo "</head>";
-?>
+<head>
+	<title>Resistance</title>
+	<meta http-equiv="Content-Language" content="en"/>
+	<?php
+		if($_SESSION['view']=="Desktop"){
+			echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"/Ingress/CSS/style.css\">";
+		}else{
+			echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"/Ingress/CSS/Mobile/style.css\">";
+		}
+	?>
+	<link rel="shortcut icon" href="/Ingress/icon.png" />
+</head>
