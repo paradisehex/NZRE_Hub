62aca6fcd309f4280f85ca5327b18d2481d76ecd
MiteyIronPaw <grayghost.ingress@gmail.com>
2013-12-30 17:21:10 +1300

    Added +1 system for verifcation

---
+++ Tools/plusOne.php
@@ -0,0 +1,20 @@
+<?php	
+	session_start();
+	
+	include $_SESSION['path']."/Tools/database.php";
+	
+	$Trustee = $_POST['Name'];
+	$Truster = $_SESSION['name'];
+	
+	$Result = selectFrom("VerifyTable", array("Truster", "Trustee"), array($Truster, $Trustee));
+	
+	$Count = mysqli_num_rows($Result);
+	
+	if($Count == 1){
+		deleteFrom("VerifyTable", array("Truster", "Trustee"), array($Truster, $Trustee));
+	}else{
+		insertCertainVaules("VerifyTable", array("Truster", "Trustee"), array($Truster, $Trustee));
+	}
+	
+	header("location:/Ingress/Users/Agents");
+?>
