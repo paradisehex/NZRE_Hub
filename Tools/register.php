12da77f5f7b0ea5f79fede984bd0b1fb192bc57e
MiteyIronPaw <grayghost.ingress@gmail.com>
2014-01-12 19:41:08 +1300

    Fixed bug in register

--- Tools/register.php
+++ Tools/register.php
@@ -4,7 +4,7 @@
 	function register($username,$password,$level){
 		$password = getHash($username,$password);
 		//Add password and user to data base
-			insertCertainVaules("AgentTable", array("username", "passwordHash", "Admin", "lvl", "AP", "Location", "InLvl", "outLvl"), array($username,$password,false,$level,0,0,7,7));
+			insertCertainVaules("AgentTable", array("username", "passwordHash", "Admin", "lvl", "AP", "Location", "ViewDegree"), array($username,$password,false,$level,0,0,2));
 			insert("ItemTable",array($username,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'Never',0));
 		//Go to tools page
 			LogText("User ".$_SESSION['name']." Registered ".$username);
