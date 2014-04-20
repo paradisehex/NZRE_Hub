233c19d8eb7f7c877d17fbfb6129e4e4c8eba7e4
MiteyIronPaw <grayghost.ingress@gmail.com>
2014-04-20 15:02:07 +1200

    Restructered CSS code

--- Tools/head.php
+++ Tools/head.php
@@ -2,11 +2,21 @@
 	<title>Resistance</title>
 	<meta http-equiv="Content-Language" content="en"/>
 	<?php
-		if($_SESSION['view']=="Desktop"){
-			echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"/Ingress/CSS/style.css\">";
-		}else{
-			echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"/Ingress/CSS/Mobile/style.css\">";
+		function CSS_link($FileName){
+			return "<link rel=\"stylesheet\" type=\"text/css\" href=\"/Ingress/CSS/".$FileName.".css\">";
 		}
+		
+		if($_SESSION['view']=="Desktop"){
+			echo CSS_link("style");
+			echo CSS_link("graph");
+			echo CSS_link("menu");
+			echo CSS_link("ap");
+			echo CSS_link("button");
+		}else{
+			echo CSS_link("Mobile/style");
+		}
+		
+		echo CSS_link("colours");
 	?>
 	<link rel="shortcut icon" href="/Ingress/icon.png" />
 </head>
