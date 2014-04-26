67c697ae6aa7a713291278dbbf882a6fc75c5c29
MiteyIronPaw <grayghost.ingress@gmail.com>
2014-04-27 00:44:58 +1200

    Fixed OP inventory sometimes counting agents twice

--- OPS/Inventory/index.php
+++ OPS/Inventory/index.php
@@ -9,11 +9,18 @@
 
 		
 		$The_OP = mysqli_fetch_array(selectFrom("OPSTable", array("Name"), array($Name)));
+		
+		if(!CanVeiwOP($TheOP['ID'], $_SESSION['name']) && $TheOP['Private']){
+			$Name = null;
+			return;
+		}
 
 		$EveryOne = selectFrom("AgentTable", null, null);
 		
 		$Total = 0;
 		$Viewable = 0;
+		
+		$Names = array();
 ?>
 <html>
 	<?php include $_SESSION['path']."/Tools/head.php";?>
@@ -37,6 +44,7 @@
 							$Items = mysqli_fetch_array($ItemQuery, MYSQL_ASSOC);
 							addInventory($Items);
 							if($Items['year']!=0){$i++;}
+							array_push($Names,$row['username']);
 						}
 					}
 				}
@@ -44,14 +52,24 @@
 				$EveryOne = selectFrom("AgentTable", null, null);
 				while ($row = mysqli_fetch_array($EveryOne, MYSQL_ASSOC)) {
 					if(0 < mysqli_num_rows(selectFrom("CoordinatorTable", array("Name","ID"), array($row['username'],$The_OP['ID'])))){
-						$Total += 1;
-						if(CanVeiwOther($row['username'])){
-							$Viewable += 1;
-							$ItemString = "SELECT * FROM ItemTable WHERE username='".$row['username']."'";
-							$ItemQuery = selectFrom("ItemTable", array("username"), array($row['username']));
-							$Items = mysqli_fetch_array($ItemQuery, MYSQL_ASSOC);
-							addInventory($Items);
-							if($Items['year']!=0){$i++;}
+						$IsRepeat = false;
+						foreach($Names as $A_Name){
+							//echo "<br>".$A_Name." , ".$row['username'];
+							if($A_Name == $row['username']){
+								$IsRepeat = true;
+							}
+						}
+						
+						if( ! $IsRepeat ){
+							$Total += 1;
+							if(CanVeiwOther($row['username'])){
+								$Viewable += 1;
+								$ItemString = "SELECT * FROM ItemTable WHERE username='".$row['username']."'";
+								$ItemQuery = selectFrom("ItemTable", array("username"), array($row['username']));
+								$Items = mysqli_fetch_array($ItemQuery, MYSQL_ASSOC);
+								addInventory($Items);
+								if($Items['year']!=0){$i++;}
+							}
 						}
 					}
 				}
