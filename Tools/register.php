e517d8ea445ef416d7bc810a6b6ff0ac4269d47a
MiteyIronPaw <grayghost.ingress@gmail.com>
2014-04-27 15:07:56 +1200

    Fixed bug with registering agents not creating inventory table

--- Tools/register.php
+++ Tools/register.php
@@ -5,7 +5,7 @@
 		$password = getHash($username,$password);
 		//Add password and user to data base
 			insertCertainVaules("AgentTable", array("username", "passwordHash", "Admin", "lvl", "AP", "Location", "ViewDegree"), array($username,$password,false,$level,0,0,2));
-			insert("ItemTable",array($username,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'Never',0));
+			insertCertainVaules("ItemTable", array("username", "day", "month", "year"), array($username,0,'Never',0));
 		//Go to tools page
 			LogText("User ".$_SESSION['name']." Registered ".$username);
 	}
